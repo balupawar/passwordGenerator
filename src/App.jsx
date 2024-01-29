a0ade1def36ca407e4a1c7b75d8f303411ee19dc
Balu <balupawar4615@gmail.com>
2024-01-29 15:23:25 +0530

    first commit for submiting password getnerator project

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import { useState,useCallback, useEffect, useRef } from 'react'
+
+
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed,setNumberAllowed] = useState(false);
+  const [charAllowed, SetCharAllowed] = useState(false);
+  const [password,setPassword] = useState("");
+// useCallback() hook
+/**
+ * useCallback is a react hook that lets you cache a function between re-renders
+ *  cosnt cacheFn = useCallback(fn, dependencies);
+ * => first argument as function
+ * => second is depndencies in array
+ */
+
+//useRef hook
+const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_-+=~1"
+
+    for(let i = 1; i <= length; i++)
+    {
+      let char =Math.floor((Math.random() * str.length + 1));
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+
+  },[length,numberAllowed, charAllowed, setPassword]);
+ 
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select()
+    //select between reanges
+    // passwordRef.current?.setSelectionRange(0,3);
+    window.navigator.clipboard.writeText(password);
+  },[password])
+  /**
+   * useEffect Hook
+   * useEffect is React Hook taht is lets you synchronize a component with an external system
+   * useEffect(callback, dependencies array)
+   */
+
+  useEffect(() =>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed,passwordGenerator])
+
+  return (
+    <>
+      
+      <div className='w-full, max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8 text-orange-500 bg-gray-800'>
+      <h1 className="text-center text-white my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+           type="text" 
+           value={password}
+           className="outline-none w-full py-1 px-3"
+           placeholder='Password'
+           readOnly
+           ref={passwordRef}/>
+           <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+           onClick={copyPasswordToClipboard}>copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range" 
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e)=>{setLength(e.target.value)}}
+            />
+            <label htmlFor="Length">Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input type="checkbox"
+            defaultChecked={numberAllowed}
+            id='numberInput'
+            onChange={()=>{
+              setNumberAllowed((prev) => !prev)
+            }} 
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input type="checkbox"
+            defaultChecked={charAllowed}
+            id='characterInput'
+            onChange={()=>{
+              SetCharAllowed((prev) => !prev)
+            }} 
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
